f2363e5dd82f7c217e2f59e77925c162da88734e
Ali Dankintafo Amadou <dankintafoamadou@gmail.com>
2018-01-19 15:58:57 +0100

    page acceuil et voir annonce

---
+++ app/src/main/java/com/a21713885/l3/unicaen/android/annonceapp/Annonce.java
@@ -0,0 +1,125 @@
+package com.a21713885.l3.unicaen.android.annonceapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by amadou on 19/01/18.
+ */
+
+public class Annonce {
+    private String id;
+    private String titre;
+    private String description;
+    private int price;
+    private String pseudo;
+    private String emailContact;
+    private String telContact;
+    private String ville;
+    private String codePostal;
+    private ArrayList<String>  images = new ArrayList<>();
+    private String date;
+
+    public Annonce(String id, String titre, String description, int price,
+                   String pseudo, String emailContact, String telContact,
+                   String ville, String codePostal, ArrayList<String> images, String date) {
+        this.id = id;
+        this.titre = titre;
+        this.description = description;
+        this.price = price;
+        this.pseudo = pseudo;
+        this.emailContact = emailContact;
+        this.telContact = telContact;
+        this.ville = ville;
+        this.codePostal = codePostal;
+        this.images = images;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getEmailContact() {
+        return emailContact;
+    }
+
+    public void setEmailContact(String emailContact) {
+        this.emailContact = emailContact;
+    }
+
+    public String getTelContact() {
+        return telContact;
+    }
+
+    public void setTelContact(String telContact) {
+        this.telContact = telContact;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public ArrayList<String> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<String> images) {
+        this.images = images;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
--- app/src/main/java/com/a21713885/l3/unicaen/android/annonceapp/MainActivity.java
+++ app/src/main/java/com/a21713885/l3/unicaen/android/annonceapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.a21713885.l3.unicaen.android.annonceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private  Button voirAnnonce;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        voirAnnonce=(Button)findViewById(R.id.voir_annonce);
+        voirAnnonce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voirAnnonceListner(view);
+            }
+        });
+    }
+
+
+    private void voirAnnonceListner(View view)
+    {
+        Intent intent = new Intent(this,VoirAnnonceActivity.class);
+        startActivity(intent);
     }
 }
