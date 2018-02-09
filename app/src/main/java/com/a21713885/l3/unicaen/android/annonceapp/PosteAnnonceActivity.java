7b9368c8466d0830da9327104f98ce9d325eb6a4
Ali Dankintafo Amadou <dankintafoamadou@gmail.com>
2018-02-09 16:12:23 +0100

    Poster une annonce avec volley et affichage de l annonce ajouté

--- app/src/main/java/com/a21713885/l3/unicaen/android/annonceapp/PosteAnnonceActivity.java
+++ app/src/main/java/com/a21713885/l3/unicaen/android/annonceapp/PosteAnnonceActivity.java
@@ -1,5 +1,6 @@
 package com.a21713885.l3.unicaen.android.annonceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -7,8 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,6 +34,7 @@ public class PosteAnnonceActivity extends AppCompatActivity {
     private EditText prix;
     private EditText test;
     private ApiInterface apiInterface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,20 +56,21 @@ public class PosteAnnonceActivity extends AppCompatActivity {
                 String desc_chaine = desc.getText().toString().trim();
                 String prix_chaine = prix.getText().toString().trim();
                 if (!TextUtils.isEmpty(titre_chaine) && !TextUtils.isEmpty(desc_chaine) && !TextUtils.isEmpty(prix_chaine)){
-                    sendAnnoncePost(titre_chaine, desc_chaine, prix_chaine);
+                    saveAnnonce(titre_chaine, desc_chaine, prix_chaine);
                 }
             }
         });
 
     }
 
-    public void sendAnnoncePost(String titre, String desc, String prix){
+    /*public void sendAnnoncePost(String titre, String desc, String prix){
         String apikey = "21713885", method= "save",pseudo="alpha",emailContact="alpha@gmail.com", telContact="0682136682",
         ville="Herouville",cp="14200";
         apiInterface.createAnnonce(apikey, method, titre, desc, prix, pseudo, emailContact,telContact,ville,cp).enqueue(new Callback<Annonce>() {
             @Override
             public void onResponse(Call<Annonce> call, Response<Annonce> response) {
-                showResponse(response.toString());
+
+                //showResponse(response.toString());
                 System.out.println("annonce sauvegardée avec succès"+response.body().toString());
             }
 
@@ -69,6 +84,68 @@ public class PosteAnnonceActivity extends AppCompatActivity {
 
     public void showResponse(String response){
 
-        test.setText(response);
+
+    }*/
+
+    public void saveAnnonce(final String titre, final String desc, final String prix) {
+        String url = "https://ensweb.users.info.unicaen.fr/android-api/";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        //String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //*******
+                        //test.setText(response.toString());
+                        Intent intent = new Intent(PosteAnnonceActivity.this,VoirAnnonceActivity.class);
+                        try {
+                            JSONObject resp = new JSONObject(response);
+                            JSONObject o = (JSONObject)resp.get("response");
+                            JSONArray img = (JSONArray)o.get("images");
+                            ArrayList<String> image = new ArrayList<>();
+                            for (int k = 0; k < img.length(); k++)
+                                image.add(img.get(k).toString());
+                            Annonce annonce = new Annonce(o.get("id").toString(), o.get("titre").toString(), o.get("description").toString(),
+                                    o.get("prix").toString(), o.get("pseudo").toString(), o.get("emailContact").toString(),
+                                    o.get("telContact").toString(), o.get("ville").toString(), o.get("cp").toString(),
+                                    image, o.get("date").toString());
+                            intent.putExtra("Annonce",annonce);
+                            startActivity(intent);
+                        } catch (Exception e){
+                            System.out.println("Erreur "+ e.getMessage());
+                        }
+
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+    }) {
+            @Override
+            protected Map<String, String> getParams() {
+                /*final String apikey="", method= "save",pseudo="alpha",emailContact="alpha@gmail.com", telContact="0682136682",
+                        ville="Herouville",cp="14200";*/
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("apikey","21713885");
+                params.put("method","save");
+                params.put("titre",titre);
+                params.put("description",desc);
+                params.put("prix",prix);
+                params.put("pseudo","alpha");
+                params.put("emailContact","alpha@gmail.com");
+                params.put("telContact","0682136682");
+                params.put("ville","Caen");
+                params.put("cp","14200");
+
+            return params;
+            }
+        };
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
